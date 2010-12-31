4e4c184c15757225092300e5bee75b730305af4c
Kiran S J <kiransj2@gmail.com>
2010-12-31 09:07:12 +0530

    added one function readIndex file, and called it where ever index file is read.
    And also added some comments in some functions

--- inc/obj.h
+++ inc/obj.h
@@ -1,8 +1,13 @@
+#ifndef _OBJ_H
+#define _OBJ_H
 #include "commit.h"
 #include "strings.h"
 bool getBranchName(String const s);
 bool setBranchName(String const s);
+
 bool getCurrentIndexFile(String s);
+bool readIndexFile(FileList index, String indexfile);
+
 bool copyFileToCache(File f);
 bool moveFileFromCacheToRepo(File f);
 bool copyFileFromRepo(File f);
@@ -13,3 +18,4 @@ bool copyTreeFromRepo(ShaBuffer tree, const char *dest, int mode);
 bool getCurrentCommit(Commit c, ShaBuffer currentCommit);
 bool setCurrentCommit(ShaBuffer commitSha);
 bool compareIndexWithWorkingArea(void);
+#endif
